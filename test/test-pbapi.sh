c39996d8c156fc08e93290be58a2b1569c09ab7e
ProfitBricks <f@moredevs7.(none)>
2012-03-13 19:52:21 +0200

    Tests now wait for data center state to become AVAILABLE after each test

--- test/test-pbapi.sh
+++ test/test-pbapi.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 debug=""
+pause=""
 ( echo "$@" | grep -e "-debug" >/dev/null ) && debug="-debug"
+( echo "$@" | grep -e "-pause" >/dev/null ) && pause="-pause"
 
 function api() {
-	../src/pbapi.py $debug "$@" 2>&1
+	../src/pbapi.py $debug "$@"
 	exit_code=$?
-	if [ "$exit_code" != "0" ]; then
-		echo "WARNING: exit code $exit_code"
+	if [ -n "$dc_id" ]; then
+		cmd="eval ../src/pbapi.py $debug get-datacenter-state -dcid $dc_id | grep 'AVAILABLE' 2>&1 >/dev/null ; echo \$?"
+		while [ "$($cmd)" != "0" ]; do
+			sleep 3
+		done
 	fi
 	return $exit_code
 }
@@ -19,16 +24,20 @@ function assert_text() {
 	output=`api $@`
 	if [ ! -z "$debug" ]; then echo $output; fi
 	( echo $output | grep -e "$condition" >/dev/null ) && ( assert_passed "$message" ) || ( assert_text_failed "$message" "$condition" "$@" )
+	next_test
 }
 
 function assert_passed() {
-	echo -e "Passed: $1\n"
+	echo "Passed: $1"
 }
 
 function assert_text_failed() {
 	message="$1" ; shift
 	condition="$1" ; shift
-	echo -e "Failed: $message\nCommand: $@\nMissing text: $condition\n"
+	echo -en "\x07"
+	echo -e "\nFailed: $message\nCommand: $@\nMissing text: $condition\n"
+	sleep 1
+	echo -en "\x07"
 }
 
 function assert_not_text() {
@@ -38,12 +47,16 @@ function assert_not_text() {
 	output=`api $@`
 	if [ ! -z "$debug" ]; then echo $output; fi
 	( echo $output | grep -e "$condition" >/dev/null ) && ( assert_not_text_failed "$message" "$condition" "$@" ) || ( assert_passed "$message" )
+	next_test
 }
 
 function assert_not_text_failed() {
 	message="$1" ; shift
 	condition="$1" ; shift
-	echo -e "Failed: $message\nCommand: $@\nFound text: $condition\n"
+	echo -en "\x07"
+	echo -e "\nFailed: $message\nCommand: $@\nFound text: $condition\n"
+	sleep 1
+	echo -en "\x07"
 }
 
 function wait_cmd() {
@@ -53,34 +66,51 @@ function wait_cmd() {
 	i=0
 	while [ $i -le 60 ]; do
 		echo -n "."
-		cmd_result=`$cmd`
-		if [ "$cmd_result" == "0" ]; then
+		if [ "$($cmd)" == "0" ]; then
 			echo " passed."
+			next_test
 			return 0
 		fi
 		sleep 3; i=$[ $i + 3 ]
 	done
 	echo "FAILED!"
+	next_test
 	return 1
+}
+
+function next_test() {
+	if [ -n "$pause" ]; then
+		echo " = press enter to continue ="
+		read
+	fi
 }
 
 ### DEFAULT VALUES ###
 
-default_dc_name="TEST-dc"
-default_srv_name="TEST-srv"
-default_sto_name="TEST-sto"
-success="Operation completed"
+name_key="test-2"
+
+default_dc_name="$name_key-dc"
+default_srv_name="$name_key-srv"
+default_sto_name="$name_key-sto"
+
+success="Operation completed" # do not change this unless it is also changed in src/pb/formatter.py
+
+### ENABLED TESTS ###
+
+test_data_center="1"
+test_virtual_server="1"
+test_storage="1"
 
 ### DATA CENTER ###
 
 create_dc_command="create-datacenter -name $default_dc_name"
-[ "0" == "1" ] && (
+[ "$test_data_center" == "1" ] && (
+	echo " === RUNNING DATA CENTER TESTS ==="
 	dc_id=`api $create_dc_command | grep 'ID:' | sed -e 's/.*ID: //'`
 	[ "$dc_id" == "" ] && ( assert_text_failed "Create DataCenter" "ID:" "$create_dc_command" ) || ( assert_passed "Create DataCenter" )
 	
 	assert_text "Get DataCenter" "$default_dc_name" get-datacenter -dcid $dc_id
 	assert_text "Get DataCenter state" "Provisioning state" get-datacenter-state -dcid $dc_id
-	wait_cmd "for data center '$dc_id' to be AVAILABLE" "api get-datacenter-state -dcid $dc_id | grep 'AVAILABLE'"
 	assert_text "Get all DataCenters" "$dc_id" get-all-datacenters
 	assert_text "Clear empty DataCenter" "$success" clear-datacenter -dcid $dc_id
 	srv_id=`api create-server -dcid $dc_id -cores 1 -ram 256 -name $default_srv_name | grep 'ID:' | sed -e 's/.*ID: //'`
@@ -89,27 +119,29 @@ create_dc_command="create-datacenter -name $default_dc_name"
 	wait_cmd "server to delete" "api get-datacenter -dcid $dc_id | grep 'Servers (0)'"
 	api update-datacenter -dcid $dc_id -name ${default_dc_name}2
 	assert_text "Rename DataCenter" "${default_dc_name}2" get-datacenter -dcid $dc_id
-	assert_text "Delete DataCenter" "$success" delete-datacenter -dcid $dc_id
-	assert_not_text "Get all DataCenters after deletion" "$dc_id" get-all-datacenters
+	old_dc_id="$dc_id"
+	dc_id="" # need to do this because the api() function waits for the datacenter $dc_id to be in AVAILABLE state after each command
+	assert_text "Delete DataCenter" "$success" delete-datacenter -dcid $old_dc_id
+	assert_not_text "Get all DataCenters after deletion" "$old_dc_id" get-all-datacenters
 )
 
 echo "Creating victim data center"
 dc_id=`api $create_dc_command | grep 'ID:' | sed -e 's/.*ID: //'`
-wait_cmd "for data center '$dc_id' to be AVAILABLE" "api get-datacenter-state -dcid $dc_id | grep 'AVAILABLE'"
 
 ### VIRTUAL SERVER ###
 
 create_srv_command="create-server -dcid $dc_id -cores 1 -ram 256 -name $default_srv_name"
-[ "0" == "1" ] && (
+[ "$test_virtual_server" == "1" ] && (
+	echo " === RUNNING VIRTUAL SERVER TESTS ==="
 	srv_id=`api $create_srv_command | grep 'ID:' | sed -e 's/.*ID: //'`
 	[ "$srv_id" == "" ] && ( assert_text_failed "Create Server" "ID:" "$create_srv_command" ) || ( assert_passed "Create Server" )
 	
 	assert_text "Get Server" "$default_srv_name" get-server -srvid "$srv_id"
 	wait_cmd "for server $srv_id to boot" "api get-server -srvid $srv_id | grep 'RUNNING'"
 	assert_text "Reboot Server" "$success" reboot-server -srvid "$srv_id"
-	wait_cmd "for server $srv_id to reboot" "api get-server -srvid $srv_id | grep 'NOSTATE'"
-	wait_cmd "for server $srv_id to boot" "api get-server -srvid $srv_id | grep 'RUNNING'"
+	wait_cmd "for server $srv_id to reboot" "api get-server -srvid $srv_id | grep 'RUNNING'"
 	assert_text "Delete Server" "$success" delete-server -srvid "$srv_id"
+	wait_cmd "for data center to be available again" "api get-datacenter-state -dcid $dc_id | grep 'AVAILABLE'"
 )
 
 echo "Creating victim server"
@@ -119,7 +151,8 @@ wait_cmd "for server '$srv_id' to boot" "api get-server -srvid $srv_id | grep 'R
 ### STORAGE ###
 
 create_sto_command="create-storage -dcid $dc_id -name $default_sto_name -size 1"
-[ "0" == "1" ] && (
+[ "$test_storage" == "1" ] && (
+	echo " === RUNNING STORAGE TESTS ==="
 	sto_id=`api $create_sto_command | grep 'ID:' | sed -e 's/.*ID: //'`
 	[ "$sto_id" == "" ] && ( assert_text_failed "Create Storage" "ID:" "$create_sto_command" ) || ( assert_passed "Create Storage" )
 	
@@ -133,17 +166,20 @@ create_sto_command="create-storage -dcid $dc_id -name $default_sto_name -size 1"
 	assert_text "Rename Storage" "${default_dc_name}2" get-storage -stoid $sto_id
 	wait_cmd "for storage $sto_id to be AVAILABLE" "api get-storage -stoid $sto_id | grep 'AVAILABLE'"
 	assert_text "Delete Storage" "$success" delete-storage -stoid $sto_id
+	wait_cmd "for data center to be available again" "api get-datacenter-state -dcid $dc_id | grep 'AVAILABLE'"
 )
 
 echo "Creating victim storage"
 sto_id=`api $create_sto_command | grep 'ID:' | sed -e 's/.*ID: //'`
 wait_cmd "for storage '$sto_id' to be AVAILABLE" "api get-storage -stoid $sto_id | grep 'AVAILABLE'"
 
-echo -e "\n\nCLEANING UP\n"
+echo -e "\n === CLEANING UP ===\n"
 
 api clear-datacenter -dcid $dc_id
-wait_cmd "servers to delete" "api get-datacenter -dcid $dc_id | grep 'Servers (0)'"
-wait_cmd "storages to delete" "api get-datacenter -dcid $dc_id | grep 'Storages (0)'"
-api delete-datacenter -dcid $dc_id
-wait_cmd "datacenter to delete" "api get-all-datacenters | awk '/$dc_id/{found=1;exit} END{exit(found)}'" # wait until text is not found
+wait_cmd "for servers to delete" "api get-datacenter -dcid $dc_id | grep 'Servers (0)'"
+wait_cmd "for storages to delete" "api get-datacenter -dcid $dc_id | grep 'Storages (0)'"
+old_dc_id="$dc_id"
+dc_id="" # need to do this because the api() function waits for the datacenter $dc_id to be in AVAILABLE state after each command
+api delete-datacenter -dcid $old_dc_id
+wait_cmd "for datacenter to delete" "api get-all-datacenters | awk '/$old_dc_id/{found=1;exit} END{exit(found)}'" # wait until text is not found
 
