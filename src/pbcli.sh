f043e6ad617ae87cd15a69af1c47642587f24f32
ProfitBricks <f@moredevs7.(none)>
2012-03-15 11:34:15 +0200

    Added 'wait' and 'use DCID' commands

--- src/pbcli.sh
+++ src/pbcli.sh
@@ -1,9 +1,42 @@
 #!/bin/bash
 
+# Helpers
 version="1.1"
-help="ProfitBricks API CLI v${version} Copyright 2012 ProfitBricks GmbH, licensed under Apache 2.0 ( http://www.apache.org/licenses/LICENSE-2.0 )\nType 'exit' to leave, 'help' for help, 'list' to list available operations or 'last' to repeat your last command."
+tbold=`tput bold`
+tnormal=`tput sgr0`
+help="${tbold}ProfitBricks API CLI v${version} Copyright 2012 ProfitBricks GmbH${tnormal}, licensed under Apache 2.0 ( http://www.apache.org/licenses/LICENSE-2.0 )\nType 'exit' to leave, 'help' for help, 'list' to list available operations or 'last' to repeat your last command."
+home=$(dirname $(readlink -f $0))
+last_command=""
+default_dc=""
+last_command=""
+prompt="ProfitBricks> "
+wait_dc=""
 
 echo -e "\n$help"
+
+function parse_internal() {
+	cmd="$1" ; shift
+	args="$@"
+	case "${cmd}" in
+		"use")
+			if [ -n "${args}" ]; then
+				default_dc="-dcid ${args}"
+				prompt="${args}> "
+				echo "Using ${args} as default datacenter"
+			else
+				default_dc=""
+				prompt="ProfitBricks> "
+				echo "Removed default datacenter"
+			fi
+			echo "-"
+			command=""
+			;;
+		"wait")
+			wait_dc="$(echo ${args::1} | tr '[A-Z]' '[a-z]')"
+			command=""
+			;;
+	esac
+}
 
 # Read default arguments
 echo ""
@@ -31,11 +64,12 @@ done
 compgen -W "$ops" "/"
 
 # Loop until 'exit'
-last=""
 while [ true ]; do
-	read -e -p "ProfitBricks> " command
+	read -e -p "${tbold}${prompt}${tnormal}" command
 	
-	case "$command" in
+	parse_internal ${command}
+	
+	case "${command}" in
 		"")
 			# ignore
 			;;
@@ -47,14 +81,20 @@ while [ true ]; do
 			break
 			;;
 		"last")
-			echo "Issuing last command: $last"
-			command=$last
+			echo "Issuing last command: $last_command"
+			command=${last_command}
 			;& # fall through
 		*)
-			last=$command
-			./pbapi.py $default_args $command ; exit_code=$?
-			if [ "$exit_code" != "0" ]; then echo "Exit code: $exit_code"; fi
-			echo ""
+			last_command=${command}
+			${home}/pbapi.py ${default_args} ${default_dc} ${command} ; exit_code=$?
+			[ "${exit_code}" != "0" ] && echo "Exit code: ${exit_code}"
+			[ "${wait_dc}" == "y" ] && [ -n "${default_dc}" ] && (
+				until [ "$(${home}/pbapi.py ${default_args} ${default_dc} get-datacenter-state | grep 'AVAILABLE\|resource does not exist' | wc -l)" -gt 0 ]; do
+					echo -n "."
+					sleep 1
+				done
+				echo ""
+			) || echo "-"
 			;;
 	esac
 done
