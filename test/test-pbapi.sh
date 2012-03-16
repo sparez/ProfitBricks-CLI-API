662808031dd2ed697ad3373b38fe1ec1a61354a2
ProfitBricks <f@moredevs7.(none)>
2012-03-16 19:40:06 +0200

    Tests wait for data center to become available by default

--- test/test-pbapi.sh
+++ test/test-pbapi.sh
@@ -6,12 +6,12 @@ pause=""
 ( echo "$@" | grep -e "-pause" >/dev/null ) && pause="-pause"
 
 function api() {
-	../src/pbapi.py ${debug} "$@"
+	../src/pbapi.py ${debug} "$@" | grep -v 'Request ID'
 	exit_code=$?
 	if [ -n "${dc_id}" ]; then
 		cmd="eval ../src/pbapi.py ${debug} get-datacenter-state -dcid ${dc_id} | grep 'AVAILABLE' 2>&1 >/dev/null ; echo \$?"
 		while [ "$(${cmd})" != "0" ]; do
-			sleep 3
+			sleep 2
 		done
 	fi
 	return ${exit_code}
@@ -98,17 +98,16 @@ success="Operation completed" # do not change this unless it is also changed in 
 ### ENABLED TESTS ###
 
 test_data_center="1"
-test_virtual_server="1"
-test_storage="1"
+test_virtual_server="0"
+test_storage="0"
 
 ### DATA CENTER ###
 
 create_dc_command="create-datacenter -name ${default_dc_name}"
 [ "${test_data_center}" == "1" ] && (
-	echo " === RUNNING DATA CENTER TESTS ==="
+	echo -e "\n === RUNNING DATA CENTER TESTS ===\n"
 	dc_id=`api ${create_dc_command} | grep 'ID:' | sed -e 's/.*ID: //'`
-	[ "${dc_id}" == "" ] && ( assert_text_failed "Create DataCenter" "ID:" "${create_dc_command}" ) || ( assert_passed "Create DataCenter" )
-	
+	[ "${dc_id}" == "" ] && ( assert_text_failed "Create DataCenter" "ID:" "${create_dc_command}" ) || ( assert_passed "Create DataCenter (${dc_id})" )
 	assert_text "Get DataCenter" "${default_dc_name}" get-datacenter -dcid ${dc_id}
 	assert_text "Get DataCenter state" "Provisioning state" get-datacenter-state -dcid ${dc_id}
 	assert_text "Get all DataCenters" "${dc_id}" get-all-datacenters
@@ -132,16 +131,15 @@ dc_id=`api ${create_dc_command} | grep 'ID:' | sed -e 's/.*ID: //'`
 
 create_srv_command="create-server -dcid ${dc_id} -cores 1 -ram 256 -name ${default_srv_name}"
 [ "${test_virtual_server}" == "1" ] && (
-	echo " === RUNNING VIRTUAL SERVER TESTS ==="
+	echo -e "\n === RUNNING VIRTUAL SERVER TESTS ===\n"
 	srv_id=`api ${create_srv_command} | grep 'ID:' | sed -e 's/.*ID: //'`
-	[ "${srv_id}" == "" ] && ( assert_text_failed "Create Server" "ID:" "${create_srv_command}" ) || ( assert_passed "Create Server" )
+	[ "${srv_id}" == "" ] && ( assert_text_failed "Create Server" "ID:" "${create_srv_command}" ) || ( assert_passed "Create Server (${srv_id})" )
 	
 	assert_text "Get Server" "${default_srv_name}" get-server -srvid "${srv_id}"
 	wait_cmd "for server ${srv_id} to boot" "api get-server -srvid ${srv_id} | grep 'RUNNING'"
 	assert_text "Reboot Server" "${success}" reboot-server -srvid "${srv_id}"
 	wait_cmd "for server ${srv_id} to reboot" "api get-server -srvid ${srv_id} | grep 'RUNNING'"
 	assert_text "Delete Server" "${success}" delete-server -srvid "${srv_id}"
-	wait_cmd "for data center to be available again" "api get-datacenter-state -dcid ${dc_id} | grep 'AVAILABLE'"
 )
 
 echo "Creating victim server"
@@ -152,9 +150,9 @@ wait_cmd "for server '${srv_id}' to boot" "api get-server -srvid ${srv_id} | gre
 
 create_sto_command="create-storage -dcid ${dc_id} -name ${default_sto_name} -size 1"
 [ "${test_storage}" == "1" ] && (
-	echo " === RUNNING STORAGE TESTS ==="
+	echo -e "\n === RUNNING STORAGE TESTS ===\n"
 	sto_id=`api ${create_sto_command} | grep 'ID:' | sed -e 's/.*ID: //'`
-	[ "${sto_id}" == "" ] && ( assert_text_failed "Create Storage" "ID:" "${create_sto_command}" ) || ( assert_passed "Create Storage" )
+	[ "${sto_id}" == "" ] && ( assert_text_failed "Create Storage" "ID:" "${create_sto_command}" ) || ( assert_passed "Create Storage (${sto_id})" )
 	
 	assert_text "Get Storage" "${default_sto_name}" get-storage -stoid "${sto_id}"
 	wait_cmd "for storage ${sto_id} to be AVAILABLE" "api get-storage -stoid ${sto_id} | grep 'AVAILABLE'"
@@ -166,12 +164,16 @@ create_sto_command="create-storage -dcid ${dc_id} -name ${default_sto_name} -siz
 	assert_text "Rename Storage" "${default_dc_name}2" get-storage -stoid ${sto_id}
 	wait_cmd "for storage ${sto_id} to be AVAILABLE" "api get-storage -stoid ${sto_id} | grep 'AVAILABLE'"
 	assert_text "Delete Storage" "${success}" delete-storage -stoid ${sto_id}
-	wait_cmd "for data center to be available again" "api get-datacenter-state -dcid ${dc_id} | grep 'AVAILABLE'"
 )
 
 echo "Creating victim storage"
 sto_id=`api ${create_sto_command} | grep 'ID:' | sed -e 's/.*ID: //'`
 wait_cmd "for storage '${sto_id}' to be AVAILABLE" "api get-storage -stoid ${sto_id} | grep 'AVAILABLE'"
+
+### LOAD BALANCER ###
+
+
+### CLEAN-UP ###
 
 echo -e "\n === CLEANING UP ===\n"
 
